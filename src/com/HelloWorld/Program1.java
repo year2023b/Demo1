438fd138dcc9637c215a7b9f7ecd3c73312d8b23
DESKTOP-2POJL1F\DELL <studentbrama>
2024-03-31 04:31:32 -0700

    second  commit

---
+++ src/com/HelloWorld/Program1.java
@@ -0,0 +1,14 @@
+package com.HelloWorld;
+
+public class Program1 {
+
+	public Program1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
